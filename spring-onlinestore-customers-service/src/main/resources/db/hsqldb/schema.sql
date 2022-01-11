be50b4beb462dcdc6d00930da13c77ec227382b2
lara.laskowsky <lara.laskowsky@student.fh-kiel.de>
2022-01-11 15:46:56 +0100

    afdas

--- spring-onlinestore-customers-service/src/main/resources/db/hsqldb/schema.sql
+++ spring-onlinestore-customers-service/src/main/resources/db/hsqldb/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS products (
   product_id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   product_name VARCHAR(30),
   product_type varchar(255),
-  product_price double,
+  product_price varchar(255),
   product_description VARCHAR(255),
   product_img_url varchar(255),
   INDEX(product_name)
