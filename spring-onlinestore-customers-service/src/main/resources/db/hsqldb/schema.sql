3eee9ce70a3c16063671e26932463567aefc2dfc
dshilja <dshilja@web.de>
2022-01-11 12:20:57 +0100

    names in service customer

--- spring-onlinestore-customers-service/src/main/resources/db/hsqldb/schema.sql
+++ spring-onlinestore-customers-service/src/main/resources/db/hsqldb/schema.sql
@@ -1,30 +1,14 @@
-DROP TABLE pets IF EXISTS;
-DROP TABLE types IF EXISTS;
-DROP TABLE owners IF EXISTS;
+CREATE DATABASE IF NOT EXISTS onlinestore;
+GRANT ALL PRIVILEGES ON onlinestore.* TO pc@localhost IDENTIFIED BY 'pc';
 
-CREATE TABLE types (
-  id   INTEGER IDENTITY PRIMARY KEY,
-  name VARCHAR(80)
-);
-CREATE INDEX types_name ON types (name);
+USE onlinestore;
 
-CREATE TABLE owners (
-  id         INTEGER IDENTITY PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name  VARCHAR(30),
-  address    VARCHAR(255),
-  city       VARCHAR(80),
-  telephone  VARCHAR(20)
-);
-CREATE INDEX owners_last_name ON owners (last_name);
-
-CREATE TABLE pets (
-  id         INTEGER IDENTITY PRIMARY KEY,
-  name       VARCHAR(30),
-  birth_date DATE,
-  type_id    INTEGER NOT NULL,
-  owner_id   INTEGER NOT NULL
-);
-ALTER TABLE pets ADD CONSTRAINT fk_pets_owners FOREIGN KEY (owner_id) REFERENCES owners (id);
-ALTER TABLE pets ADD CONSTRAINT fk_pets_types FOREIGN KEY (type_id) REFERENCES types (id);
-CREATE INDEX pets_name ON pets (name);
+CREATE TABLE IF NOT EXISTS products (
+  product_id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  product_name VARCHAR(30),
+  product_type varchar(255),
+  product_price double,
+  product_description VARCHAR(255),
+  product_img_url varchar(255),
+  INDEX(product_name)
+) engine=InnoDB;
