4de9568dec2da887e814b7465c95f055eb003384
lara.laskowsky <lara.laskowsky@student.fh-kiel.de>
2022-01-11 15:50:54 +0100

    aesdf

--- spring-onlinestore-customers-service/src/main/java/org/springframework/samples/onlinestore/customers/model/Product.java
+++ spring-onlinestore-customers-service/src/main/java/org/springframework/samples/onlinestore/customers/model/Product.java
@@ -46,7 +46,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
     @Column(name = "product_name")
     @NotEmpty
