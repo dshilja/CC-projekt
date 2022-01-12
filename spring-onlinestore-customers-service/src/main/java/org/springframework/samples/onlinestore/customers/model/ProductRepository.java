27935e1383eb82a5cdd9a95663dda56afdda37da
lara.laskowsky <lara.laskowsky@student.fh-kiel.de>
2022-01-12 16:08:49 +0100

    add files

--- spring-onlinestore-customers-service/src/main/java/org/springframework/samples/onlinestore/customers/model/ProductRepository.java
+++
@@ -1,5 +0,0 @@
-package org.springframework.samples.onlinestore.customers.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> { }
